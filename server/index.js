24822046bfd9a3338dc12e4287f36a3952f08596
Subodh Gangwar <subodhgangwar0123@gmail.com>
2025-06-25 16:59:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,9 +8,21 @@ require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
 // Middleware
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://news-together-app-1.onrender.com'
+];
+
 app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
